c4d4eafcbd28406670e37873d8eff3bee563b054
Anupam <anupaminit@gmail.com>
2025-07-24 19:14:17 +0530

    feat: kzb-21 update the script

--- src/scripts/01_variables/bashsol_01.sh
+++ src/scripts/01_variables/bashsol_01.sh
@@ -1,1 +1,11 @@
-echo "Hello world...!!!"
+# The shebang is the first line in a shell script that tells the operating system which interpreter to use to run the script.
+# Without it, the system may default to sh (which is more limited than bash). It ensures consistent behavior across systems.
+# If you run a script with bash script.sh, the shebang is ignored. If you run it as ./script.sh, the shebang is honored.
+
+#!/bin/bash
+
+# A variable stores data that can be used later in the script. No space on either side of =
+# Use snake_case for regular variable names and UPPERCASE for env variables.
+
+display_message="Hello world!"
+echo "The message: ${display_message}"
